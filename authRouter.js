f5860708c77c379a5471a991c7d9bc798377a7ff
lol <lol>
2025-04-10 19:42:10 +0300

    NEw LOck3

--- authRouter.js
+++ authRouter.js
@@ -6,6 +6,7 @@ router.post('/registration', controller.registation)
 router.post('/login', controller.login)
 router.post('/setStan', controller.Stan)
 router.post('/product',controller.product)
+router.post('setLock',controller.setLock)
 
 router.get('/getStan', controller.getStan)
 router.get('/getProduct', controller.getProduct)
